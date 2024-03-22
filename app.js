dce47770539da49a9ed459e7b0c294f85f17c75c
nmatos <nmatos@pursuit.org>
2024-03-22 14:33:42 -0400

    fixed some tyling, fixed my javascript up a bit to match the styling on css

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function addBook() {
-    //Retrieveing Data
+    //Creating Data
     const book = { 
         title: document.getElementById('title').value,
         author: document.getElementById('author').value,
-        imageBook: document.getElementById('urlSite').src,
+        imageBook: document.getElementById('urlSite').value,
         inventory: document.getElementById('bookInventory__inStock').value,
         price: document.getElementById('price').value
     }
@@ -14,32 +14,52 @@ function addBook() {
     localStorage.setItem('books', JSON.stringify(books));
 
     getBooks();
-}
+    
 
+}
+//Retrieving Data
 function getBooks() {
+    document.getElementById('bookInventory__listedBooks').innerHTML = '';
+
     const booksTwo = JSON.parse(localStorage.getItem('books')) || []
    
     if(booksTwo.length > 0) {
-        document.getElementById('bookInventory__listedBooks'),textContent = '';
          booksTwo.forEach((b) => {
-            const bookListed = document.createElement('div');
-            bookListed.innerHTML = 
-                `<p>${b.title}</p>
+            const bookInfo = document.createElement('div')
+            const imageBook = document.createElement('img');
+            const title = document.createElement('div');
+            const author = document.createElement('div');
+            const stock = document.createElement('div');
+            const price = document.createElement('div');
+            const deleteBox = document.createElement('input')
+            bookInfo.setAttribute('class','bookInventory__bookInfo')
+            imageBook.setAttribute('src', b.imageBook);
+            title.setAttribute('class', 'bookInventory__title');
+            author.setAttribute('class', 'bookInventory__author');
+            stock.setAttribute('class', 'bookInventory__stock');
+            price.setAttribute('class', 'bookInventory__price');
+            deleteBox.setAttribute('class', 'checkBox');
+            bookInfo.innerHTML = 
+                `<img src="${b.imageBook}" alt="Book Image">
+                <p class>${b.title}</p>
                 <p>${b.author}</p>
-                <img src="${b.imageBook}" alt="Book Image">
                 <p>${b.inventory}</p>
                 <p>${b.price}</p>
-                <input type="checkbox" name="checkbox">
-                `
-
+                <input type="checkbox" name="checkBox">`
             ;
-           document.getElementById('bookInventory__listedBooks').appendChild(bookListed)
+           document.getElementById('bookInventory__listedBooks').append(bookInfo)
         })
 } else {
-    console.log('No Books')
+    console.log('No Books');
 }
 }
 
-getBooks();
+//Deleting Data
 
+function deleteBooks() {
+
+}
+
+
+window.onload = getBooks();
 
